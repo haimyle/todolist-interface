cbd21edd2f1856cd3f92a122aa58ce66a7858f1b
HaimyLe <H.le@lprs.fr>
2022-04-07 16:09:35 +0200

    feat: add method to inscription

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -6,12 +6,31 @@ import modele.User;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UserRepository {
     private Bdd cnx;
 
     public UserRepository() {
         this.cnx = new Bdd();
+    }
+
+    public void inscription (User user) throws SQLException {
+        String sql = "INSERT INTO user ( `nom`, `prenom`, `mail`,`mdp`,`est_admin`) VALUES (?,?,?,md5(?),?)";
+
+        PreparedStatement req = cnx.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        req.setString(1, user.getNom());
+        req.setString(2, user.getPrenom());
+        req.setString(3, user.getEmail());
+        req.setString(4, user.getPassword());
+        req.setBoolean(5, user.isEstAdmin());
+        req.executeUpdate();
+        ResultSet rs = req.getGeneratedKeys();
+        if(rs.next())
+        {
+            int last_inserted_id = rs.getInt(1);
+            user.setIdUser(last_inserted_id);
+        }
     }
 
     public User connexion(String email, String password) {

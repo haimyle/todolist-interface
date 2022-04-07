d03cb808518722ee635598bfcaa0060e64ad070a
HaimyLe <H.le@lprs.fr>
2022-04-07 09:17:07 +0200

    feat: add Tache model

---
+++ src/main/java/modele/Tache.java
@@ -0,0 +1,63 @@
+package modele;
+
+import java.sql.Date;
+
+public class Tache {
+    private int idTache;
+    private String nomTache;
+    private boolean terminee;
+    private Date deadline;
+    private int retfType;
+    private int refListe;
+
+    public Tache() {
+    }
+
+    public int getIdTache() {
+        return idTache;
+    }
+
+    public void setIdTache(int idTache) {
+        this.idTache = idTache;
+    }
+
+    public String getNomTache() {
+        return nomTache;
+    }
+
+    public void setNomTache(String nomTache) {
+        this.nomTache = nomTache;
+    }
+
+    public boolean isTerminee() {
+        return terminee;
+    }
+
+    public void setTerminee(boolean terminee) {
+        this.terminee = terminee;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public int getRetfType() {
+        return retfType;
+    }
+
+    public void setRetfType(int retfType) {
+        this.retfType = retfType;
+    }
+
+    public int getRefListe() {
+        return refListe;
+    }
+
+    public void setRefListe(int refListe) {
+        this.refListe = refListe;
+    }
+}

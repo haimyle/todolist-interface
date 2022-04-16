af7c7fd7b6d70b606ee6bdaa283ac20db1dd739b
HaimyLe <H.le@lprs.fr>
2022-04-16 17:06:34 +0200

    feat: change scene

---
+++ src/main/java/appli/user/AccueilController.java
@@ -0,0 +1,20 @@
+package appli.user;
+
+import appli.StartApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AccueilController {
+
+    @FXML
+    private Button btnSeConnecter;
+
+    @FXML
+    void clickConnexion(ActionEvent event) {
+        StartApplication.changeScene("/appli/user/connexion-view.fxml",new ConnexionController(),"To-Do List - Connexion");
+    }
+
+
+
+}

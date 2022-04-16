65c285b34b276977e2ffd81758d167102506994f
HaimyLe <H.le@lprs.fr>
2022-04-16 08:10:44 +0200

    feat: add modifier and supprimer button

--- src/main/java/appli/todolist/AddListController.java
+++ src/main/java/appli/todolist/AddListController.java
@@ -1,18 +1,88 @@
 package appli.todolist;
 
+import appli.StartApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import modele.Liste;
+import modele.User;
+import repository.ListeRepository;
+import repository.UserRepository;
 
-public class AddList {
+import java.sql.SQLException;
+
+public class AddListController {
+    private User user;
     @FXML
-    private Button btnSauvegarder;
+    private Button btnCancel;
 
     @FXML
-    private TextField tfNomList;
+    private Button btnSave;
 
     @FXML
-    void clickSauvegarder(ActionEvent event) {
+    private Label lbMessage;
+
+    @FXML
+    private Label lbArchives;
+
+    @FXML
+    private Label lbCategories;
+
+    @FXML
+    private Label lbCorbeille;
+
+    @FXML
+    private Label lbInbox;
+
+    @FXML
+    private Label lbListes;
+
+    @FXML
+    private Label lbToday;
+    @FXML
+    private TextField tfNomListe;
+
+    @FXML
+    void clickCancel(ActionEvent event) {
+        StartApplication.changeScene("/appli/todolist/todolist-view.fxml",new TodolistController(user),"To-Do List - Listes");
+    }
+
+    @FXML
+    void clickShowArchive(MouseEvent event) {
 
     }
+
+    @FXML
+    void clickShowCategories(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clickShowCorbeille(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clickshowListes(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clickSave(ActionEvent event) throws SQLException {
+        ListeRepository listeRepository = new ListeRepository();
+        Liste liste = listeRepository.addList(tfNomListe.getText(),user);
+        if (liste != null) {
+            StartApplication.changeScene("/appli/todolist/todolist-view.fxml", new TodolistController(user), "To-Do List - Listes");
+        } else {
+            lbMessage.setText("Erreur");
+        }
+    }
+
+
+    public AddListController(User user) {
+        this.user = user;
+    }
+}

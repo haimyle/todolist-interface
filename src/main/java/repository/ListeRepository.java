653703b625b84dc269c1ade5611e9792cd5c6834
HaimyLe <H.le@lprs.fr>
2022-04-16 08:16:02 +0200

    feat: method to insert lists and fetch lists from database

--- src/main/java/repository/ListeRepository.java
+++ src/main/java/repository/ListeRepository.java
@@ -1,40 +1,57 @@
 package repository;
 
 import database.Bdd;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import modele.Liste;
 import modele.User;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
-public class ListRepository {
+public class ListeRepository {
     private Bdd cnx;
     private Liste liste;
     private ArrayList <Liste> listes;
 
-    public ListRepository() {
+    public ListeRepository() {
         this.cnx = new Bdd();
     }
 
-    public ArrayList showList(User user) throws SQLException {
+    public Liste addList(String titre, User user) throws SQLException {
+        Liste liste = new Liste();
+        String sql = "INSERT INTO liste (nom_liste, ref_user) VALUE(?,?)";
+        PreparedStatement req = cnx.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        req.setString(1,titre);
+        req.setInt(2,user.getIdUser());
+        req.executeUpdate();
+        ResultSet res = req.getGeneratedKeys();
+        if (res.next()){
+            liste.setIdListe(res.getInt(1));
+            System.out.println("Sucess");
+        }else {
+            System.out.println("Erreur");
+        }
+        return liste;
+    }
+
+    public ArrayList<Liste> showList(User user) throws SQLException {
+        ArrayList<Liste> listes = new ArrayList<Liste>();
+        Liste liste;
         String sql = "SELECT * FROM liste WHERE ref_user =?";
         PreparedStatement req = cnx.getConnection().prepareStatement(sql);
         req.setInt(1,user.getIdUser());
         ResultSet res = req.executeQuery();
-        if (res.next()==false){
-            String message="Vide";
+        while (res.next()){
+            liste = new Liste();
+            liste.setIdListe(res.getInt("id_liste"));
+            liste.setNomListe(res.getString("nom_liste"));
+            listes.add(liste);
         }
-        else{
-            do{
-                this.listes = new ArrayList<Liste>();
-                Liste liste = new Liste();
-                liste.setIdListe(res.getInt("id_liste"));
-                liste.setNomListe(res.getString("nom_liste"));
-                this.listes.add(liste);
-            } while (res.next());
-        }
+
         return listes;
     }
 

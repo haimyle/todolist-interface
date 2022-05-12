dc8643397ae83792a34a91a1f36ab9d1b103c2b0
HaimyLe <H.le@lprs.fr>
2022-05-12 18:06:59 +0200

    feat: method create and update Type

--- src/main/java/repository/TypeRepository.java
+++ src/main/java/repository/TypeRepository.java
@@ -1,2 +1,80 @@
-package repository;public class TypeRepository {
+package repository;
+
+import database.Bdd;
+import modele.Type;
+import modele.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class TypeRepository {
+    private Bdd cnx;
+    private Type type;
+
+    public TypeRepository() {
+        this.cnx = new Bdd();
+    }
+
+    public Type createTypeWithSousType(String nomType, User user, int refType) throws SQLException {
+        type = new Type();
+        String sql = "INSERT INTO type (nom_type, ref_type, ref_user) VALUE(?,?,?)";
+        PreparedStatement req = cnx.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        req.setString(1,nomType);
+        req.setInt(2,user.getIdUser());
+        req.setInt(3,refType);
+        req.executeUpdate();
+        ResultSet res = req.getGeneratedKeys();
+        if (res.next()){
+            type.setIdType(res.getInt(1));
+            System.out.println("Sucess");
+        }else {
+            System.out.println("Erreur");
+        }
+        return type;
+    }
+
+    public Type createTypeWithoutSousType(String nomType, User user) throws SQLException {
+        type = new Type();
+        String sql = "INSERT INTO type (nom_type, ref_user) VALUE(?,?)";
+        PreparedStatement req = cnx.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        req.setString(1,nomType);
+        req.setInt(2,user.getIdUser());
+        req.executeUpdate();
+        ResultSet res = req.getGeneratedKeys();
+        if (res.next()){
+            type.setIdType(res.getInt(1));
+            System.out.println("Sucess");
+        }else {
+            System.out.println("Erreur");
+        }
+        return type;
+    }
+
+    public ArrayList<Type> readType(User user) throws SQLException {
+        ArrayList<Type> types= new ArrayList<Type>();
+        Type type;
+        String sql = "SELECT * FROM type WHERE ref_user = ?";
+        PreparedStatement req = cnx.getConnection().prepareStatement(sql);
+        req.setInt(1,user.getIdUser());
+        ResultSet res = req.executeQuery();
+        while (res.next()){
+            type = new Type();
+            type.setIdType(res.getInt("id_type"));
+            type.setNomType(res.getString("nom_type"));
+            type.setRefType(res.getInt("ref_type"));
+            types.add(type);
+        }
+        return types;
+    }
+    public void updateType(String nomType) throws SQLException {
+        String sql = "UPDATE type SET type=? WHERE type=?";
+        PreparedStatement req = cnx.getConnection().prepareStatement(sql);
+        req.setString(1,nomType);
+        //req.setInt(2,list.getIdListe());
+        req.executeUpdate();
+    }
+    public void deleteType(){}
 }

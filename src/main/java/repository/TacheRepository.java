1281fde995faf877120fb6551db017438475219d
HaimyLe <H.le@lprs.fr>
2022-05-13 14:43:21 +0200

    feat: method to insert Taches into the database

--- src/main/java/repository/TacheRepository.java
+++ src/main/java/repository/TacheRepository.java
@@ -2,10 +2,10 @@ package repository;
 
 import database.Bdd;
 import modele.Liste;
+import modele.Tache;
 import modele.User;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TacheRepository {
     private Bdd cnx;
@@ -20,5 +20,24 @@ public class TacheRepository {
         req.setInt(1,liste.getIdListe());
         req.setInt(2, user.getIdUser());
         req.executeUpdate();
+    }
+
+    public Tache createTache(String titre, Date deadline, int ref_type, int ref_liste) throws SQLException {
+        Tache tache = new Tache();
+        String sql = "INSERT INTO tache (nom_tache, deadline, ref_type, ref_liste) VALUE(?,?,?,?)";
+        PreparedStatement req = cnx.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        req.setString(1,titre);
+        req.setDate(2, deadline);
+        req.setInt(3,ref_type);
+        req.setInt(4,ref_liste);
+        req.executeUpdate();
+        ResultSet res = req.getGeneratedKeys();
+        if (res.next()){
+            tache.setIdTache(res.getInt(1));
+            System.out.println("Sucess");
+        }else {
+            System.out.println("Erreur");
+        }
+        return tache;
     }
 }

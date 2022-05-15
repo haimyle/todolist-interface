f31455c056d5365d1bd4a68e327e03b3b5c43cb7
HaimyLe <H.le@lprs.fr>
2022-05-15 23:19:42 +0200

    feat: update tache controller

---
+++ src/main/java/appli/tache/UpdateTacheController.java
@@ -0,0 +1,101 @@
+package appli.tache;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class UpdateTacheController {
+
+    @FXML
+    private Button btnArchives;
+
+    @FXML
+    private Button btnCorbeille;
+
+    @FXML
+    private Button btnListes;
+
+    @FXML
+    private Button btnTaches;
+
+    @FXML
+    private Button btnType;
+
+    @FXML
+    private ComboBox<?> comboBoxListe;
+
+    @FXML
+    private ComboBox<?> comboBoxType;
+
+    @FXML
+    private DatePicker deadline;
+
+    @FXML
+    private Label lblAdded;
+
+    @FXML
+    private Label lblError;
+
+    @FXML
+    private TextField tfTitre;
+
+    @FXML
+    void clickAnnuler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickArchives(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickCorbeille(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickListes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickSauvegarder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickTaches(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickType(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCompteAdmin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCompteModifier(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionToDoListAPropos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionToDoListDocumentation(ActionEvent event) {
+
+    }
+
+}
